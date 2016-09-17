bc909e50b9e388b9030cf8a5a80b26645af384e6
Rafael Hengles <rhengles@gmail.com>
2016-09-17 13:36:34 -0300

    Removed istanbul

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
-import babel from 'rollup-plugin-babel';
+//,import babel from 'rollup-plugin-babel';
+import buble from 'rollup-plugin-buble';
 import babelrc from 'babelrc-rollup';
 import istanbul from 'rollup-plugin-istanbul';
 
@@ -8,22 +9,23 @@ let external = Object.keys(pkg.dependencies);
 export default {
   entry: 'lib/index.js',
   plugins: [
-    babel(babelrc()),
-    istanbul({
-      exclude: ['test/**/*', 'node_modules/**/*']
-    })
+    //,babel(babelrc()),
+    buble()
+    //,istanbul({
+    //,  exclude: ['test/**/*', 'node_modules/**/*']
+    //,})
   ],
   external: external,
   targets: [
     {
       dest: pkg['main'],
       format: 'cjs',
-      sourceMap: true
+      sourceMap: false
     },
     {
       dest: pkg['jsnext:main'],
       format: 'es',
-      sourceMap: true
+      sourceMap: false
     }
   ]
 };
